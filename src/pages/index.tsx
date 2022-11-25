2a3ccf0d8845d7a2948d4ce5113d49898474d409
Chukwuma Okoroji <chuck@Chucks-MBP.hsd1.fl.comcast.net>
2022-11-24 19:34:35 -0500

    removed duplicate import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { Navbar } from '@modules/navbar/components/Navbar';
-import { Navbar } from "../modules/navbar/components/Navbar"
 import React, { useState } from "react";
 
 type TechnologyCardProps = {
